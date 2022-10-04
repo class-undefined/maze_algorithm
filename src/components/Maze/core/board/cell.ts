39d6f2afeb4ee397d325bc0d4d7ad907ce3298bc
野漫横江 <1399226280@qq.com>
2022-10-04 14:50:57 +0800

    feat: 给`Cell`赋予回溯状态的功能

--- src/components/Maze/core/board/cell.ts
+++ src/components/Maze/core/board/cell.ts
@@ -3,10 +3,19 @@ import { Cell, CellType, Pos } from "../types"
 export class MazeCell implements Cell {
     public passable: boolean
     public type: CellType
-
+    private statuStack: Cell[]
     constructor(passable?: boolean, type?: CellType) {
         this.passable = passable ?? true
         this.type = type ?? CellType.Road
+        this.statuStack = [{ passable: this.passable, type: this.type }]
+    }
+
+    public changeStatus(passable?: boolean, type?: CellType) {
+        if (!passable && !type) return this
+        this.passable = passable ?? this.passable
+        this.type = type ?? this.type
+        this.statuStack.push({ passable: this.passable, type: this.type })
+        return this
     }
 
     public static Road() {
@@ -18,13 +27,23 @@ export class MazeCell implements Cell {
     }
 
     public toRoad() {
-        this.passable = true
-        this.type = CellType.Road
-        return this
+        return this.changeStatus(true, CellType.Road)
     }
 
     public toWall() {
-        this.passable = false
-        this.type = CellType.Wall
+        return this.changeStatus(false, CellType.Wall)
+    }
+
+    /**
+     * 将自身状态回溯到上一次的状态
+     * @returns 返回自身
+     */
+    public backtrack() {
+        this.statuStack.pop()
+        if (this.statuStack.length === 0) throw "当前时刻仅有一个状态, 上一个状态为空, 不可回溯"
+        const { passable, type } = this.statuStack[this.statuStack.length - 1]
+        this.passable = passable
+        this.type = type
+        return this
     }
 }
