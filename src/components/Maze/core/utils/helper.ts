3a9282d4e2a98dc828a59664a4aa1b1d1b074a7c
野漫横江 <1399226280@qq.com>
2022-10-05 00:08:55 +0800

    feat: `Maze`的`helper`工具类`MazeHelper`

---
+++ src/components/Maze/core/utils/helper.ts
@@ -0,0 +1,25 @@
+import { Maze } from "../maze"
+import { Pos } from "../types"
+
+export class MazeHelper {
+    private constructor(private maze: Maze) {}
+
+    public static from(maze: Maze) {
+        return new MazeHelper(maze)
+    }
+
+    /**
+     * 获取鼠标所在矩形的索引
+     * @param x 棋盘`x`轴坐标
+     * @param y 棋盘`y`轴坐标
+     * @returns
+     */
+    public getRectPos(x: number, y: number): Pos {
+        const options = this.maze.getOptison()
+        const lineWidth = options.grid.lineStyle.width
+        const { width, height } = options.grid.unit
+        const rowIndex = Math.floor(x / (width + lineWidth))
+        const colIndex = Math.floor(y / (height + lineWidth))
+        return [rowIndex, colIndex]
+    }
+}
