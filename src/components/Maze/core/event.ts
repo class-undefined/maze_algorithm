2d1ed2a1d4eed1ff22e81ed0f0d71b0b4173302d
野漫横江 <1399226280@qq.com>
2022-10-05 14:22:27 +0800

    feat: 增加点击事件扩展api

--- src/components/Maze/core/event.ts
+++ src/components/Maze/core/event.ts
@@ -2,8 +2,12 @@ import { Container, DisplayObject } from "pixi.js"
 import { Maze } from "./maze"
 import { MazeStyleOptions } from "./options"
 
+export type ClickHandler = (rowIndex: number, colIndex: number) => void
 export class BorderEventSystem {
-    private constructor(private maze: Maze, private border: DisplayObject) {}
+    private clickHandlers: ClickHandler[]
+    private constructor(private maze: Maze, private border: DisplayObject) {
+        this.clickHandlers = []
+    }
 
     public static from(maze: Maze, border: DisplayObject) {
         return new BorderEventSystem(maze, border)
@@ -16,7 +20,7 @@ export class BorderEventSystem {
             const x = e.data.global.x - padding - lineWidth
             const y = e.data.global.y - padding - lineWidth
             if (x < 0 || y < 0) return
-            console.log(x, y, this.maze.helper.getRectPos(x, y))
+            this.clickHandlers.forEach(handler => handler(...this.maze.helper.getRectPos(x, y)))
         })
     }
 
@@ -24,5 +28,10 @@ export class BorderEventSystem {
         this.border.interactive = true
         this.__enableClickHandle()
         return this
+    }
+
+    /** 增加点击事件 */
+    public addClickListener(handler: ClickHandler) {
+        this.clickHandlers.push(handler)
     }
 }
